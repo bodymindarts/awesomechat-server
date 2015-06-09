02f4c7ad9c9e39a62e5fdf761401bf19f320eaad
Justin Carter <bodymindarts@online.de>
2015-06-09 07:34:11 +0200

    remove outdated specs

--- spec/chat_room_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/chat_room'
-
-RSpec.describe ChatRoom do
-  it 'broadcasts a message' do
-    clients = double('client_list')
-    expect(clients).to receive(:broadcast).with('MESSAGE')
-    ChatRoom.new(clients).receive_message('MESSAGE')
-  end
-end
